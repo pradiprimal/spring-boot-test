3c4581fdbd7b1a8223565668f2c506dd74a48f07
pradip <rimalpradip31@gmail.com>
2017-03-02 09:27:54 +0545

    spring boot application complete

--- spring-boot-start/src/main/java/spring/boot/SpringBootStarter.java
+++ spring-boot-start/src/main/java/spring/boot/SpringBootStarter.java
@@ -2,15 +2,22 @@ package spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  *
  * @author Pradip
  */
 @SpringBootApplication
-public class SpringBootStarter {
+public class SpringBootStarter extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarter.class, args);
+    }
+    
+     @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootStarter.class);
     }
 }
--- spring-boot-start/src/main/java/spring/boot/starter/api/TestApi.java
+++ spring-boot-start/src/main/java/spring/boot/starter/api/TestApi.java
@@ -1,8 +1,10 @@
 package spring.boot.starter.api;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.boot.entity.Test;
 import spring.boot.repositories.TestRepository;
 
 /**
@@ -15,8 +17,17 @@ public class TestApi {
     @Autowired
     private TestRepository repositoryImpl;
 
-    @GetMapping("/test")
-    public String getName() {
-        return "pradip";
+    @GetMapping("/save")
+    public String saveTest() {
+        Test test = new Test();
+        test.setName("test");
+        repositoryImpl.save(test);
+        return "Successfully Added";
+    }
+
+    @GetMapping("/get")
+    public List<Test> getTest() {
+        return repositoryImpl.findAll();
+
     }
 }
